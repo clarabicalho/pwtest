d95b5a5d02e0519ff06e055ca07ad1d1cb9305d4
Clara Bicalho <cbm343@nyu.edu>
2024-01-05 14:10:02 -0800

    Update package functions and metadata files

---
+++ R/helpers.R
@@ -0,0 +1,230 @@
+# Clean and standardize data before calculating coefficients of prognosis
+# regression on control units
+# Returns vector of labelled covariate (prognosis) weights
+
+pw <- function(data, covariates, treatment, outcome, standardize, simulation){
+
+  # listwise deletion of observations with missing values
+  data_pw <- data %>% drop_na(all_of(c(outcome, covariates, treatment)))
+  z0 <- data_pw[[treatment]] == 0
+
+  # standardize control data for prognosis regression
+  if(standardize){
+    if(simulation & length(unique(data[[outcome]]))==1L){
+      # standardize -covariates only- in simulation runs with fixed POs
+      data_c <- data_pw %>% filter(z0) %>%
+        mutate_at(.vars = c(covariates),
+                  .funs = stdr) %>% as.data.frame()
+    } else {
+      data_c <- data_pw %>% filter(z0) %>%
+        mutate_at(.vars = c(outcome, covariates),
+                  .funs = stdr) %>% as.data.frame()
+    }
+
+  } else {
+    data_c <- data_pw %>% filter(z0)
+  }
+
+  # check if variance = 0 and return error
+  check_var <- data_c %>%
+    dplyr::select(all_of(c(covariates, outcome))) %>%
+    apply(., 2, var, na.rm = TRUE)
+  var_na <- names(check_var[is.na(check_var)])
+  if(length(var_na)>=1L) stop(paste0("The following variables are constant in the control group, so cannot be standardized: ",
+                                     paste0(var_na, collapse = ", "),
+                                     ". Consider an alternative, for example, excluding the covariate(s)."))
+  # calculate prognosis weights
+  X <- as.matrix(data_c[,covariates], ncol = length(covariates))
+  Y <- as.matrix(data_c[,outcome], ncol = 1)
+  pw <- coef(lm(Y ~ X-1))
+
+  names(pw) <- covariates
+  return(pw)
+}
+
+#' Calculates unweighted delta
+#' @param data data.frame containing covariates, treatment assignment, and outcome variable
+#' @param covariates character vector of covariate names
+#' @param treatment name of variable indicating (binary) treatment assigned
+#' @param outcome name of outcome variable
+#' @param standardize whether to standardize data inside function
+
+uw_delta <- function(data, covariates, treatment, outcome, standardize = TRUE, simulation = FALSE){
+
+  # drop if missing treatment value
+  data_uw <- data %>% drop_na(all_of(c(treatment)))
+  z0 <- data_uw[[treatment]] == 0
+
+  # standardize data relative to entire study group (the finite population)
+  if(standardize){
+    if(simulation & length(unique(data[[outcome]]))==1L){
+      data_uw <- data_uw %>%
+        mutate_at(.vars = c(covariates),
+                  .funs = stdr) %>% as.data.frame()
+    } else {
+      data_uw <- data_uw %>%
+        mutate_at(.vars = c(outcome, covariates),
+                  .funs = stdr) %>% as.data.frame()
+    }
+  }
+
+  # covariate-by-covariate difference in means
+  DIM <- sapply(covariates, function(x){
+    d <- mean(data_uw[!z0, x], na.rm = TRUE) -
+      mean(data_uw[z0, x], na.rm = TRUE)
+    return(DIM = d)
+  })
+  # names(DIM) <- paste0("dim_", names(DIM))
+  # unweighted delta
+  uwdelta <- sum(DIM)
+
+  # check if any of the DIM are missing or NaN
+  cov_nan <- names(DIM)[is.nan(DIM)]
+  if(any(is.nan(DIM))) stop(paste0("The following covariates have no observations in either treatment or control conditions or covariate values are constant across both groups (so cannot be standardized): ",
+                                   paste(cov_nan, collapse = ", "), ". Consider removing these covariates."))
+
+  # NOTE: list-wise deletion to get analytic standard error
+  data_uwc <- data_uw[complete.cases(data_uw[,covariates]),]
+  Nc <- nrow(data_uwc) # changed from data_uw
+  z0c <- data_uwc[[treatment]] == 0
+  n1c <- sum(!z0c, na.rm = TRUE)
+  n0c <- sum(z0c, na.rm = TRUE)
+
+  # analytic standard error of unweighted delta
+  if(length(covariates)>1L){
+
+    varcov <- cov(data_uwc[,covariates], use = "everything")
+    sum_sigma2 <- sum(diag(varcov)*(nrow(data_uwc)-1)/nrow(data_uwc))
+    sum_covs <- sum(varcov[upper.tri(varcov)]*(nrow(data_uwc)-1)/nrow(data_uwc))
+    multiplier <- ((Nc^2)/(Nc-1))/(n1c*n0c)
+
+    # analytic SE
+    uwdelta_se <- sqrt(multiplier*sum(sum_sigma2, 2*sum_covs))
+
+  } else { # when only one covariate
+    uwdelta_se <- sqrt(var(data_uwc[!z0c,covariates])/n1c +
+                         var(data_uwc[z0c,covariates])/n0c)
+  }
+
+  return(list(dim = DIM,
+              uwdelta = uwdelta,
+              uwdelta_se = uwdelta_se))
+}
+
+#' Calculates prognosis-weighted delta
+#' @param data data.frame containing covariates, treatment assignment, and outcome variable
+#' @param covariates character vector of covariate names
+#' @param treatment name of variable indicating (binary) treatment assigned
+#' @param outcome name of outcome variable
+#' @param standardize whether to standardize data inside function
+#' @param DIM difference in covariate means across treatment and control from `uw_delta()`
+
+pw_delta <- function(data, covariates, treatment, outcome, standardize = TRUE,
+                     DIM, simulation = FALSE){
+
+  pweights <- pw(data, covariates, treatment, outcome, standardize, simulation)
+  pwdelta_j <- pweights*DIM
+  # names(pwdelta_j) <- paste0("pw_", covariates)
+
+  # prognosis weighted delta
+  pwdelta <- sum(pweights*DIM, na.rm = TRUE)
+
+  return(list(pw = pweights,
+              pwdelta_j = pwdelta_j,
+              pwdelta = pwdelta))
+
+}
+
+#' Calculates prognosis-weighted delta for RD designs
+#' @param data data.frame containing covariates, treatment assignment, and outcome variable
+#' @param covariates character vector of covariate names
+#' @param running_var character string with running variable name
+#' @param treatment name of variable indicating (binary) treatment assigned
+#' @param outcome name of outcome variable
+#' @param standardize whether to standardize data inside function
+#' @param simulation
+#' @param rd_estimator character. Whether to use the conventional ("h") or the bias-corrected local-polynomial point estimator ("b"). See `rdrobust()` for more details. Defaults to conventional estimate ("h").
+#' @param ... arguments passed on to `rdrobust` function. If `rdrobust()` arguments `y` and `covs` are not specified, they will take the values of the variables defined by `outcome` and `covariates`, respectively. All other arguments, if not specified, will take the default values in `rdrobust()`
+
+# REVIEW:
+# Allowing argument "c" to be passed onto pwtest() (to be used in rdrobust()) is creating all sorts of eval problems because it overrides the concatenate function `c()`
+# Need to figure out if there is a workaround (presumably so because `c()` is used in `rdrobust`)
+
+pw_delta_rdd <- function(data, covariates, running_var, treatment, outcome,
+                         standardize = TRUE, simulation = FALSE,
+                         rd_estimator = "h", ...){
+
+  argg <- as.list(match.call())
+  c <- NULL
+
+  if("c" %in% names(argg)){
+    warning("Arguments `treatment` and `c` both specified, will use `treatment` var to define treatment condition, but `c` will be passed onto `rdrobust()`. Please ensure the values coded in `treatment` are consistent with value of `c`.")
+  }
+
+  # obtain prognostic weights/coefficients for each covariate calculated for
+  # all control units in the full data
+  pweights <- pw(data = data, covariates = covariates, treatment = treatment,
+                 outcome = outcome, standardize = standardize, simulation = simulation)
+
+  # standardize data relative to entire study group (the finite population)
+  # uses same standardization procedure for data as in non-RD case
+  if(standardize){
+    if(simulation & length(unique(data[[outcome]]))==1L){
+      data <- data %>%
+        mutate_at(.vars = c(covariates),
+                  .funs = stdr) %>% as.data.frame()
+    } else {
+      data <- data %>%
+        mutate_at(.vars = c(outcome, covariates),
+                  .funs = stdr) %>% as.data.frame()
+    }
+  }
+
+  # fitted values of Y0 with prognosis weights
+  # (estimated coefs from control group regression of Y0 on covariates)
+  Y0hat <- as.matrix(data[,covariates])%*%as.matrix(pweights, nrow = 2)
+
+  # code values for rdrobust arguments
+  if(!"y" %in% names(argg)) argg$y <- Y0hat
+  if(!"x" %in% names(argg)) argg$x <- data[[running_var]]
+
+  # rdrobust() inherits arguments from pwtest()
+  rd_argg <- intersect(names(argg), names(formals(rdrobust)))
+  # REVIEW: rdrobust does not take variables in `covariates` for the argument `covs`
+  # unless `covs` is specified (separately)
+  rd_out <- do.call("rdrobust", args = argg[rd_argg])
+
+  # we use the rdrobust output to extract bandwidth, cutoff points,
+  # polynomial specifications, etc and the default of these arguments
+  # when appropriate
+
+  # pw delta as difference in intercepts for Y0hat
+  if(rd_estimator == "h") pwdelta <- unname(rd_out$Estimate[,"tau.us"])
+  if(rd_estimator == "b") pwdelta <- unname(rd_out$Estimate[,"tau.bc"])
+
+  # UW delta estimates using the optimal or user-set bandwidth
+  if(!rd_estimator %in% names(argg)){
+    # Note: if not specified, the conventional or bias-corrected bandwidth passed onto covariate-by-covariate difference in intercepts is taken from the same data used to calculate the prognosis weighted delta
+    argg[[rd_estimator]] <- unname(rd_out$bws[rd_estimator,])
+  }
+
+  # Note: calculates the difference in intercepts for covariates using the same bandwidth set by user or defaulted in rdrobust() with the fitted Y0hat.
+  # All other values are rdrobust defaults if not set by user.
+  dii_covs <- sapply(covariates, function(covariate){
+    argg_cov <- argg
+    argg_cov$y <- data[[covariate]]
+    argg_new <- intersect(names(argg), names(formals(rdrobust)))
+    rd <- do.call("rdrobust", args = argg_cov[argg_new])
+    if(rd_estimator == "h") uwd <- unname(rd$Estimate[,"tau.us"])
+    if(rd_estimator == "b") uwd <- unname(rd$Estimate[,"tau.bc"])
+    return(uwd)
+  })
+
+  return(list(dii = dii_covs,
+              uwdelta = sum(dii_covs),
+              pw = pweights,
+              pwdelta = pwdelta,
+              pwdelta_se = rd_out$se,
+              rdrobust_output = rd_out))
+
+}
---
+++ R/pwtest.R
@@ -0,0 +1,307 @@
+#' Produces unweighted and prognosis-weighted test statistics with standard errors and p-values
+#' @param data data.frame containing covariates, treatment assignment, and outcome variable
+#' @param covariates character vector of names of placebo variables. This is not to be confused with `covs` argument passed onto `rdrobust` in the case of RD designs, which can be specified separately (`...`).
+#' @param treatment name of variable indicating (binary) treatment assigned
+#' @param running_var character string with running variable name. Ignored unless `rdd = TRUE`.
+#' @param outcome name of outcome variable
+#' @param nsims numeric scalar indicating number of bootstraps to use for the resampling-based p-values
+#' @param oversample logical value. If `FALSE`, function gives resampling-based p-values using number of working bootstrap samples (which did not return an error) and prints a warning when that sample is smaller than the value in `nsims`. If `oversample = TRUE`, function continues to re-sample until number of working bootstrap samples reaches `nsims`.
+#' @param se_type character string. Determines which type of standard error should be returned with the unweighted delta estimate. Takes the values of either `"analytic"` or `"bootstrap"` for non RDD cases, and can also take values "conventional", "bias-adjusted", and "robust" standard errors returned by `rdrobust` (when `rdd = TRUE`). If set to "analytic" in the latter, default results will use conventional standard errors.
+#' @param simulation logical value.
+#' @param rdd logical value. Whether test statistics are calculated using continuous RDD approach.
+#' @param rd_estimator character. Whether to use the conventional ("h") or the bias-corrected local-polynomial point estimator ("b"). See `rdrobust()` for more details. Defaults to conventional estimate ("h").
+#' @param ... arguments passed onto `rdrobust()` function
+#' @import rdrobust
+#' @export
+
+pwtest <- function(data,
+                   covariates = c("X1", "X2", "X3"),
+                   treatment = "Z", running_var = NULL,
+                   outcome = "Y",
+                   nsims = 500, oversample = FALSE,
+                   se_type = "analytic",
+                   simulation = FALSE, rdd = FALSE,
+                   rd_estimator = "h", ...){
+
+  require(rdrobust)
+  argg <- as.list(match.call())
+  c <- NULL
+
+  # REVIEW write warning if specifying differing values for y and outcome, x and treatment
+  # write message if set se_type = 'analytic' and rdd = TRUE
+  if(rdd & is.null(running_var)) stop("When using 'rdd = TRUE', need to specify 'running_var'.")
+
+  if(!rdd & (length(se_type) != 1L |
+             !se_type %in% c("analytic", "bootstrap"))) stop("`se_type` must take be set to either 'analytic' or 'bootstrap'.")
+
+  if(rdd & (length(se_type) != 1L |
+            !se_type %in% c("analytic", "bootstrap", "conventional", "bias-corrected", "robust"))) stop("`se_type` must take be set to either 'analytic', 'bootstrap', 'conventional', 'bias-corrected', 'robust'.")
+
+  # restrict data to variables of interest
+  data <- data %>% dplyr::select(all_of(c(treatment, covariates, running_var, outcome)))
+
+  # observed -------------------------------------
+
+  if(!rdd){
+    uwdelta_obs <- uw_delta(data, covariates, treatment, outcome, standardize = TRUE,
+                            simulation = simulation)
+    pwdelta_obs <- pw_delta(data, covariates, treatment, outcome, standardize = TRUE,
+                            DIM = uwdelta_obs$dim,
+                            simulation = simulation)
+  }else{
+    argg$standardize <- TRUE
+    argg_def <- formals()
+    missing_args <- names(argg_def)[!names(argg_def) %in% names(argg)]
+    for(missing in missing_args){
+      argg[[missing]] <- argg_def[[missing]]
+    }
+    pwdelta_obs <- do.call("pw_delta_rdd", args = argg)
+
+    # pwdelta_obs <- pw_delta_rdd(data, covariates, running_var, treatment, outcome,
+    #                             standardize = TRUE, simulation = simulation,
+    #                             rd_estimator = rd_estimator, ...)
+  }
+
+  # check that no NA coefficients
+  # coeflabs <- grep("coef_", names(pwdelta_obs), value = TRUE)
+  coef_na <- is.na(pwdelta_obs$pw)
+  if(any(coef_na)){
+    vars <- names(which(coef_na))
+    warning(paste0("The following variables return missing coefficients in regression Y[Z=0] ~ X[Z=0]: ", vars, ". Consider omitting those variables for pwdelta."))
+  }
+
+  # bootstrapping -------------------------------------
+
+  # resample from control group with replacement
+  treat_i <- which(data[[treatment]]==1)
+  control_i <- which(data[[treatment]]==0)
+  samples <- replicate(
+    nsims,
+    c(sample(control_i, length(treat_i), replace = TRUE),
+      sample(control_i, length(control_i), replace = TRUE)))
+
+  # standardize bootstrap population relative to control group SD
+  data_stdc <- data %>%
+    mutate_at(.vars = c(outcome, covariates),
+              .funs = function(x){
+                x_c <- x[data[[treatment]]==0]
+                sd_x <- sd(x_c, na.rm = TRUE)*(sum(!is.na(x_c))-1)/sum(!is.na(x_c))
+                return((x - mean(x_c, na.rm = TRUE))/sd_x)}) %>%
+    as.data.frame()
+
+  # obtain delta distribution from bootstrap samples
+  delta_sim <- apply(samples, 2, function(z){
+    dat_uw <- data_stdc[z,]
+    # change treatment condition from control to treatment for bootstrap treatment group
+    dat_uw[[treatment]][1:length(treat_i)] <- 1
+
+    dat_pw <- data[z,]
+    # change treatment condition from control to treatment for bootstrap treatment group
+    dat_pw[[treatment]][1:length(treat_i)] <- 1
+
+    if(!rdd){
+      uwdelta_sim <- tryCatch(
+        expr = {uw_delta(dat_uw, covariates, treatment, outcome, standardize = FALSE)},
+        error = function(e) {
+          out <- vector(mode = "list", length = length(uwdelta_obs))
+          names(out) <- names(uwdelta_obs)
+          return(out)
+          message("Error with UW delta estimation of bootstrap sample.")
+        }
+      )
+
+      pwdelta_sim <- tryCatch(
+        expr = {pw_delta(dat_pw, covariates, treatment, outcome, standardize = TRUE,
+                         DIM = uwdelta_sim$dim,
+                         simulation = simulation)},
+        error = function(e) {
+          out <- vector(mode = "list", length = length(pwdelta_obs))
+          names(out) <- names(pwdelta_obs)
+          return(out)
+          # REVIEW line below not printing with results
+          message("Error with pw delta estimation of bootstrap sample.")
+        }
+      )
+
+      return(c(uwdelta_sim, pwdelta_sim))
+
+    }else{
+      # cutoff takes default value as in rdrobust() if not specified by user
+      cutoff <- ifelse("c" %in% names(argg), argg$c, 0)
+      # invert the running variable for the bootstrap sample of treatment observations
+      bs_t <- dat_pw[[treatment]] == 1
+      dat_pw[bs_t, running_var] <- -(dat_pw[bs_t, running_var] - cutoff)
+
+      pwdelta_sim <- tryCatch(
+        expr = {pwdelta_obs <- do.call("pw_delta_rdd", args = argg)},
+        error = function(e) {
+          out <- rep(NA, length(pwdelta_obs))
+          names(out) <- names(pwdelta_obs)
+          return(out)
+          # REVIEW  line below not printing with results
+          # message("Error with pw delta estimation of bootstrap sample.")
+        }
+      )
+      return(c(pwdelta_sim))
+    }
+
+  })
+
+  # bootstrap sample checks ----------------------------------
+
+  effective_sample <- min(sum(!is.na(sapply(delta_sim, function(e) e$uwdelta))),
+                          sum(!is.na(sapply(delta_sim, function(e) e$pwdelta))))
+
+  if(!identical(effective_sample, as.integer(nsims)) & !oversample)
+    warning(paste0("Effective bootstrap sample to calculate delta p-values is of size ", effective_sample, ". Consider changing argument `oversample` to `TRUE`"))
+
+  if(oversample &
+     (!identical(effective_sample, as.integer(nsims)))){
+
+    # re-sample until effective is equal or greater to bootstrap sample size argument
+    while(effective_sample < as.integer(nsims)){
+
+      samples <- replicate(
+        nsims,
+        c(sample(control_i, length(treat_i), replace = TRUE),
+          sample(control_i, length(control_i), replace = TRUE)))
+
+      # restrict bootstrap sample to complete observations
+
+      # obtain delta distribution based on resamples
+      delta_sim_add <- apply(samples, 2, function(z){
+        dat_uw <- data_stdc[z,]
+        # change treatment condition from control to treatment for sample
+        dat_uw[[treatment]][1:length(treat_i)] <- 1
+
+        dat_pw <- data[z,]
+        # change treatment condition from control to treatment for sample
+        dat_pw[[treatment]][1:length(treat_i)] <- 1
+
+        if(!rdd){
+          uwdelta_sim <- tryCatch(
+            expr = {uw_delta(dat_uw, covariates, treatment, outcome, standardize = FALSE)},
+            error = function(e) {
+              out <- vector(mode = "list", length = length(uwdelta_obs))
+              names(out) <- names(uwdelta_obs)
+              return(out)
+              message("Error with UW delta estimation of bootstrap sample.")
+            }
+          )
+
+          pwdelta_sim <- tryCatch(
+            expr = {pw_delta(dat_pw, covariates, treatment, outcome, standardize = TRUE,
+                             DIM = uwdelta_sim$dim,
+                             simulation = simulation)},
+            error = function(e) {
+              out <- vector(mode = "list", length = length(pwdelta_obs))
+              names(out) <- names(pwdelta_obs)
+              return(out)
+              # REVIEW below not printing with results
+              message("Error with pw delta estimation of bootstrap sample.")
+            }
+          )
+
+          return(c(uwdelta_sim, pwdelta_sim))
+        }else{
+
+          # RD DESIGN ############
+          # cutoff takes default value as in rdrobust() if not specified by user
+          cutoff <- ifelse("c" %in% names(argg), argg$c, 0)
+          # invert the running variable for the bootstrap sample of treatment observations
+          bs_t <- dat_pw[[treatment]] == 1
+          dat_pw[bs_t, running_var] <- -(dat_pw[bs_t, running_var] - cutoff)
+
+          pwdelta_sim <- tryCatch(
+            expr = {pw_delta_rdd(dat_pw, covariates, running_var, treatment, outcome,
+                                 rd_estimator, standardize = TRUE, simulation = simulation)},
+            error = function(e) {
+              out <- rep(NA, length(pwdelta_obs))
+              names(out) <- names(pwdelta_obs)
+              return(out)
+              # REVIEW line below not printing with results
+              # message("Error with pw delta estimation of bootstrap sample.")
+            }
+          )
+          return(pwdelta_sim)
+        }
+
+      })
+
+      delta_sim <- c(delta_sim, delta_sim_add)
+
+      # #combine list elements into one
+      # rdouts <- which(unlist(lapply(delta_sim, names)) == "rdrobust_output")
+      # delta_all <- with(stack(
+      #   lapply(delta_sim, function(i) i[!names(i) %in% "rdrobust_output"])
+      # ), split(values, ind))
+
+      na_check <- sapply(delta_sim, function(e) !is.na(e$uwdelta) & !is.na(e$pwdelta))
+      delta_sim <- delta_sim[na_check]
+      effective_sample <- sum(na_check)
+
+    }
+    # if bootstrap sample greater than argument, randomly select samples to match sample size argument
+    if(effective_sample > as.integer(nsims))
+      delta_sim <- delta_sim[sample(1:length(delta_sim), nsims, replace = FALSE)]
+  }
+
+  # SEs as SD of sampling distribution
+  uwdelta_se2 <- sd(unlist(sapply(delta_sim, function(x) x$uwdelta)), na.rm = TRUE)*(nsims-1)/nsims
+  pwdelta_se <- sd(unlist(sapply(delta_sim, function(x) x$pwdelta)), na.rm = TRUE)*(nsims-1)/nsims
+
+  # p-value (from bootstrap distribution)
+  pw_delta_p <- sum(abs(unlist(sapply(delta_sim, function(x) x$pwdelta))) >= abs(pwdelta_obs$pwdelta), na.rm = TRUE)/effective_sample
+
+  if(rdd & se_type %in% c("analytic", "conventional", "bias-corrected", "robust")){
+
+    pwdelta_se <- switch(se_type,
+                         analytic = pwdelta_obs$rd_out$se["Conventional",],
+                         conventional = pwdelta_obs$rd_out$se["Conventional",],
+                         `bias-corrected` = pwdelta_obs$rd_out$se["Bias-Corrected",],
+                         robust = pwdelta_obs$rd_out$se["Robust",],
+    )
+    # p-value (from analytic SE)
+    pw_delta_t <- pwdelta_obs$pwdelta/pwdelta_se
+    pw_delta_p <- 2*pnorm(-abs(pw_delta_t))
+  }
+
+  # R-squared from prognosis regression
+  prog_mod_f <- paste(c(outcome, paste(covariates, collapse = " + ")), collapse = " ~ ")
+  prog_mod <- lm(formula = prog_mod_f, data = data[data[[substitute(treatment)]]==0,])
+  prog_Rsq <- summary(prog_mod)$r.squared
+
+  # output ------------------------------------------------------------------
+
+  if(!rdd){
+    estimates <- c(
+      uwdelta_obs[-length(uwdelta_obs)],
+      uwdelta_se = switch(se_type, analytic = uwdelta_obs$uwdelta_se, bootstrap = uwdelta_se2),
+      uwdelta_p =
+        sum(abs(unlist(sapply(delta_sim, function(x) x$uwdelta))) >= abs(uwdelta_obs$uwdelta))/effective_sample,
+      pwdelta_obs,
+      pwdelta_se = unname(pwdelta_se),
+      pwdelta_p =
+        sum(abs(unlist(sapply(delta_sim, function(x) x$pwdelta))) >= abs(pwdelta_obs$pwdelta), na.rm = TRUE)/effective_sample,
+      prog_Rsq = prog_Rsq
+    )
+  }else{
+    estimates <- list(
+      dii = pwdelta_obs$dii,
+      uwdelta = pwdelta_obs$uwdelta,
+      uwdelta_se = uwdelta_se2,
+      uwdelta_p =
+        sum(abs(unlist(sapply(delta_sim, function(x) x$uwdelta))) >= abs(pwdelta_obs$uwdelta))/effective_sample,
+      pw = pwdelta_obs$pw,
+      pwdelta = pwdelta_obs$pwdelta,
+      pwdelta_se = pwdelta_se,
+      pwdelta_p = pw_delta_p,
+      rdrobust_output = pwdelta_obs$rdrobust_output,
+      prog_Rsq = prog_Rsq
+    )
+  }
+
+
+  return(estimates)
+
+}
